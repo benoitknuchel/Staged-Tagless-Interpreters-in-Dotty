1e244115630566a6b0616c266b35f5c1667254e1
Benoit Knuchel <benoit.knuchel@epfl.ch>
2018-09-24 18:08:59 +0200

    managing exceptions but getting not understable errors: unresolved symbol and ArrayOutOfBounds

--- simplestagedinterpreter/src/main/scala/Main.scala
+++ simplestagedinterpreter/src/main/scala/Main.scala
@@ -25,41 +25,89 @@ object Main {
   import Def._
   import Prog._
 
-  //Environments bases, empty -> throw NoSuchElementException
-  // A: Expr[Int] or Expr[Int => Int]
-  def env0[A](s: String): A = throw new NoSuchElementException
-  def fenv0[A](s: String): A = throw new NoSuchElementException
+  //Environments bases, empty -> None
+  def env0(s: String): Expr[Option[Int]] = '(None)
+  def fenv0(s: String): Expr[Int => Option[Int]] = '((x: Int) => None)
 
+  //[A]: Expr[Option[Int]] or Expr[Int => Option[Int]]
   def ext[A](env: (String => A), s: String, v: A): String => A = {
     y: String => if(s == y) v else env(y)
   }
 
 
   //The evaluator
-  def eval1(e: Exp, env: String => Expr[Int], fenv: String => Expr[Int => Int]): Expr[Int] = e match {
-    case int(x) => x.toExpr
-    case Var(s) => env(s)
-    case App(s, e) => '{ (~fenv(s))(~eval1(e, env, fenv)) }
-    case Add(e1, e2) => '{ ~eval1(e1, env, fenv) + ~eval1(e2, env, fenv) }
-    case Sub(e1, e2) => '{ ~eval1(e1, env, fenv) - ~eval1(e2, env, fenv) }
-    case Mul(e1, e2) => '{ ~eval1(e1, env, fenv) * ~eval1(e2, env, fenv) }
-    case Div(e1, e2) => '{ ~eval1(e1, env, fenv) / ~eval1(e2, env, fenv) }
+  def eval(e: Exp, env: String => Expr[Option[Int]], fenv: String => Expr[Int => Option[Int]]): Expr[Option[Int]] = e match {
+    case int(x) => '(Some(x)) //doesn't recognize x ("unresolved symbol"), compile if I write '(Some(1)) but still get an ArrayOutOfBoundsException
+    case Var(s) => '(~env(s))
+    case App(s, e) => '{
+      ~eval(e, env, fenv) match {
+        case Some(x) => (~fenv(s))(x)
+        case _ => None
+      }
+    }
+    case Add(e1, e2) => '{
+      (~eval(e1, env, fenv), ~eval(e2, env, fenv)) match {
+        case (Some(x), Some(y)) => Some(x+y)
+        case _ => None
+      }
+    }
+    case Sub(e1, e2) => '{
+      (~eval(e1, env, fenv), ~eval(e2, env, fenv)) match {
+        case (Some(x), Some(y)) => Some(x-y)
+        case _ => None
+      }
+    }
+    case Mul(e1, e2) => '{
+      (~eval(e1, env, fenv), ~eval(e2, env, fenv)) match {
+        case (Some(x), Some(y)) => Some(x*y)
+        case _ => None
+      }
+    }
+    case Div(e1, e2) => '{
+      (~eval(e1, env, fenv), ~eval(e2, env, fenv)) match {
+        case (Some(x), Some(y)) => if(y != 0) Some(x/y) else None
+        case _ => None
+      }
+    }
     case Ifz(e1, e2, e3) => '{
-      if(~eval1(e1, env, fenv) == 0) ~eval1(e2, env, fenv)
-      else ~eval1(e3, env, fenv)
+      ~eval(e1, env, fenv) match {
+        case Some(x) => if(x == 0) ~eval(e2, env, fenv) else ~eval(e3, env, fenv)
+        case _ => None
+      }
     }
   }
 
-  def peval1(p: Prog, env: String => Expr[Int], fenv: String => Expr[Int => Int]): Expr[Int] = p match {
-    case Program(Nil, e) => eval1(e, env, fenv)
+  def peval(p: Prog, env: String => Expr[Option[Int]], fenv: String => Expr[Int => Option[Int]]): Expr[Option[Int]] = p match {
+    case Program(Nil, e) => eval(e, env, fenv)
+    //recursively eval each declaration
     case Program(Declaration(s1, s2, e1)::tl, e) => '{
-      lazy val f: Int => Int = (x: Int) => ~eval1(e1, ext(env, s2, '(x)), ext(fenv, s1, '(f)))
-      ~peval1(Program(tl, e), env, ext(fenv, s1, '(f))) //why also extend fenv ?
+      lazy val f: Int => Option[Int] = (x: Int) => ~eval(e1, ext(env, s2, '(Some(x))), ext(fenv, s1, '(f)))
+      ~peval(Program(tl, e), env, ext(fenv, s1, '(f)))
     }
   }
 
 
   def main(args: Array[String]): Unit = {
+
+    //Some examples
+    
+    val first = Var("x")//int(1: Int)
+    val firstRes = eval(first, env0, fenv0)
+    println("=================")
+    println("run : " + firstRes.run)
+    println("show : " + firstRes.show)
+    println("=================")
+
+
+    val a = int(0)
+    val b = Add(int(1), Mul(int(2), int(3)))
+    val p = Ifz(a, int(2), b)
+    val snd = eval(b , env0, fenv0)
+    println("=================")
+    println("run : " + snd.run)
+    println("show : " + snd.show)
+    println("=================")
+
 
     val factorial = Program(List(Declaration
                             ("fact", "x", Ifz(Var("x"),
@@ -69,29 +117,12 @@ object Main {
                           , Declaration("twoTimesFact", "y", Mul(int(2), App("fact", Var("y"))))),
                           App("twoTimesFact", int(5)))
 
-    val res = peval1(factorial, env0, fenv0)
+    val res = peval(factorial, env0, fenv0)
     println("=================")
+    println("The program comes here and then crash, 'factorial' is already evaluated")
     println("run : " + res.run)
     println("show : " + res.show)
     println("=================")
-
-    val a = int(0)
-    val b = Add(int(1), Mul(int(2), int(3)))
-    val p = Ifz(a, int(2), b)
-    val snd = eval1(p , env0, fenv0)
-    println("=================")
-    println("run : " + snd.run)
-    println("show : " + snd.show)
-    println("=================")
-
-    val first = int(1: Int)
-    val firstRes = eval1(first, env0, fenv0)
-    println("=================")
-    println("run : " + firstRes.run)
-    println("show : " + firstRes.show)
-    println("=================")
-
-
 
   }
 }
