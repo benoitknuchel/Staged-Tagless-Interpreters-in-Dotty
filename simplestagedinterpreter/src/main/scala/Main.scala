a6a1068eca0e4ff3156d1e4775b56d1f1a7c98db
Benoit Knuchel <benoit.knuchel@epfl.ch>
2018-09-24 08:24:47 +0200

    simplestagedinterpreter working

--- simplestagedinterpreter/src/main/scala/Main.scala
+++ simplestagedinterpreter/src/main/scala/Main.scala
@@ -52,56 +52,45 @@ object Main {
 
   def peval1(p: Prog, env: String => Expr[Int], fenv: String => Expr[Int => Int]): Expr[Int] = p match {
     case Program(Nil, e) => eval1(e, env, fenv)
-    //I want to translate this in scala: Program (Declaration (s1,s2,e1)::tl,e) ->
-    //                                      .<let rec f x = .~(eval2 e1 (ext env s2 .<x>.)
-    //                                                      (ext fenv s1 .<f>.))
-    //                                      in .~(peval2 (Program(tl,e)) env (ext fenv s1 .<f>.))>.
     case Program(Declaration(s1, s2, e1)::tl, e) => '{
-      //s1 = e1(s2)
-      //The problem here is to bind s1 to e1 (name to body) using the extension environment function 'ext'
-      //Have to eval e1 and extend fenv by mapping s1 to e1 but they depend on one another
-      //That's why it is not possible to pass '(f) to the ext funtion (compiler error : "could not find proxy for val f...")
-      def f(x: Int): Int = ~eval1(e1, ext(env, s2, '(x)), ext(fenv, s1, '(f)))
-      ~peval1(Program(tl, e), env, fenv)
+      lazy val f: Int => Int = (x: Int) => ~eval1(e1, ext(env, s2, '(x)), ext(fenv, s1, '(f)))
+      ~peval1(Program(tl, e), env, ext(fenv, s1, '(f))) //why also extend fenv ?
     }
   }
 
 
   def main(args: Array[String]): Unit = {
 
-    val programFactorial = Program(List(Declaration
+    val factorial = Program(List(Declaration
                             ("fact", "x", Ifz(Var("x"),
                                               int(1),
                                               Mul(Var("x"),
                                                   (App("fact", Sub(Var("x"), int(1)))))))
-                          ),
-                          App("fact", int(10)))
+                          , Declaration("twoTimesFact", "y", Mul(int(2), App("fact", Var("y"))))),
+                          App("twoTimesFact", int(5)))
 
-    val res = peval1(programFactorial, env0, fenv0)
+    val res = peval1(factorial, env0, fenv0)
     println("=================")
     println("run : " + res.run)
     println("show : " + res.show)
     println("=================")
 
-    /* These two examples work if I replace "ext(fenv, s1, '(f))" by "fenv" because we don't need to extend fenv...
     val a = int(0)
     val b = Add(int(1), Mul(int(2), int(3)))
     val p = Ifz(a, int(2), b)
     val snd = eval1(p , env0, fenv0)
     println("=================")
-    println("run : " + snd.run)cakeaucottage12
-
+    println("run : " + snd.run)
     println("show : " + snd.show)
     println("=================")
 
-    //works
     val first = int(1: Int)
     val firstRes = eval1(first, env0, fenv0)
     println("=================")
     println("run : " + firstRes.run)
     println("show : " + firstRes.show)
     println("=================")
-    */
+
 
 
   }
