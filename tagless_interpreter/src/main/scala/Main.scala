c6639c1e8b8be04f4d4f1ed268618f2fce67c2f2
Benoit Knuchel <benoit.knuchel@epfl.ch>
2018-10-16 17:19:26 +0200

    corrected the StackOverflowError

--- tagless_interpreter/src/main/scala/Main.scala
+++ tagless_interpreter/src/main/scala/Main.scala
@@ -7,12 +7,12 @@ trait Symantics[repr[_]] {
 
   def lam[A: Type, B: Type](f: repr[A] => repr[B]): repr[A => B]
   def app[A, B](f: repr[A => B], arg: repr[A]): repr[B]
-  def fix[A, B](f: repr[A => B] => repr[A => B]): repr[A => B]
+  def fix[A](f: repr[A => A] => repr[A => A]): repr[A => A]
 
   def add(x: repr[Int], y: repr[Int]): repr[Int]
   def mul(x: repr[Int], y: repr[Int]): repr[Int]
   def leq(x: repr[Int], y: repr[Int]): repr[Boolean]
-  def if_[A](cond: repr[Boolean], e1: repr[A], e2: repr[A]): repr[A]
+  def if_[A](cond: repr[Boolean], e1: => repr[A], e2: => repr[A]): repr[A]
 }
 
 object Main {
@@ -26,12 +26,12 @@ object Main {
 
     override def lam[A: Type, B: Type](f: A => B): A => B = f
     override def app[A, B](f: A => B, arg: A): B = f(arg)
-    override def fix[A, B](f: (A => B) => (A => B)): A => B = f(fix(f))(_)
+    override def fix[A](f: (A => A) => (A => A)): A => A = f(fix(f))(_: A) //(x: A) => f(fix(f))(x)
 
     override def add(x: Int, y: Int): Int = x + y
     override def mul(x: Int, y: Int): Int = x * y
     override def leq(x: Int, y: Int): Boolean = x <= y
-    override def if_[A](cond: Boolean, e1: A, e2: A): A = if (cond) e1 else e2
+    override def if_[A](cond: Boolean, e1: => A, e2: => A): A = if (cond) e1 else e2
   }
 
 
@@ -40,14 +40,15 @@ object Main {
     override def int(x: Int): Expr[Int] = x.toExpr
     override def bool(b: Boolean): Expr[Boolean] = b.toExpr
 
-    override def lam[A: Type, B: Type](f: Expr[A] => Expr[B]): Expr[A => B] =  '{ (x: A) => ~(f('(x))) } // = f.reflect() as said in "A Practical Unification of macros Multi-stage Programming and Macros" 6) Staged Lambdas
-    override def app[A, B](f: Expr[A => B], arg: Expr[A]): Expr[B] = f(arg) //'{ (~f)(~arg) } //use .asFunction()
-    override def fix[A, B](f: Expr[A => B] => Expr[A => B]): Expr[A => B] = f(fix(f))
+    override def lam[A: Type, B: Type](f: Expr[A] => Expr[B]): Expr[A => B] =  '{ (x: A) => ~(f('(x))) } // = f.reflect() ?? as said in "A Practical Unification of macros Multi-stage Programming and Macros 6) Staged Lambdas"
+    override def app[A, B](f: Expr[A => B], arg: Expr[A]): Expr[B] = f(arg) //'{ (~f)(~arg) }, use .asFunction()
+    override def fix[A](f: Expr[A => A] => Expr[A => A]): Expr[A => A] = f(fix(f)) //'{ ~f(fix(f))(_: A) }
 
     override def add(x: Expr[Int], y: Expr[Int]): Expr[Int] = '{ ~x + ~y }
     override def mul(x: Expr[Int], y: Expr[Int]): Expr[Int] = '{ ~x * ~y }
     override def leq(x: Expr[Int], y: Expr[Int]): Expr[Boolean] = '{ ~x <= ~y }
-    override def if_[A](cond: Expr[Boolean], e1: Expr[A], e2: Expr[A]): Expr[A] = '{ if(~cond) ~e1 else ~e2 }
+    //e1 and e2 must be CBN because of 'fix' : if either e1 or e2 is a recursion then' it'll be evaluated immediately and so on -> infinite loop
+    override def if_[A](cond: Expr[Boolean], e1: => Expr[A], e2: => Expr[A]): Expr[A] = '{ if(~cond) ~e1 else ~e2 }
   }
 
 
@@ -88,9 +89,9 @@ object Main {
     ////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////
-
+    /**
     //TEST ID
-    /*
+
     import eval._
 
     //(b=b)(true)
@@ -111,10 +112,8 @@ object Main {
     println("res : " + t3)
     println("======================")
 
-    TODO: fix the StackOverflowError coming from t4 (hypothesis : 'if_' isn't recognised as a stop for recursion)
-    //factorial(5) //StackOverFlowError when using if_, does the compiler see an infinite loop ?
-    //val t4 = app(fix((f: Int => Int) => (n: Int) => if_(leq(n, int(1)), n, mul(f(add(n, -1)), n))), int(5)) //this doesn't work
-    val t4 = app(fix((f: Int => Int) => (n: Int) => if(leq(n, int(1))) n else mul(f(add(n, -1)), n)), int(5)) //this works
+    //factorial(5)
+    val t4 = app(fix((f: Int => Int) => (n: Int) => if_(leq(n, int(1)), n, mul(f(add(n, -1)), n))), int(5))
     println("res : " + t4)
     println("======================")
     */
